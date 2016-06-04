7dd66be1fc918385eb0cf4bb3741d7ec7af5af5f
Christina <christina@highlig.ht>
2016-06-04 13:52:18 -0700

    Ability to ask for Sheets read/write perm

--- app/src/main/java/com/christina/tracker/MainActivity.kt
+++ app/src/main/java/com/christina/tracker/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.christina.tracker
 
+import android.accounts.AccountManager
 import com.christina.tracker.R
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 import android.support.v4.view.PagerAdapter
@@ -13,6 +15,12 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
+import com.google.api.client.util.ExponentialBackOff
+import com.google.api.services.sheets.v4.SheetsScopes
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
 import rx.Observable
 import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
@@ -24,16 +32,103 @@ import timber.log.Timber
  * Created by christina on 2/7/16.
  */
 
-public class MainActivity: Activity() {
-  override fun onCreate(savedInstanceBundle: Bundle?) {
-    super.onCreate(savedInstanceBundle)
+public class MainActivity: Activity(), EasyPermissions.PermissionCallbacks {
+    companion object {
+        const val REQUEST_ACCOUNT_PICKER = 1000
+        const val REQUEST_AUTHORIZATION = 1001
+        const val REQUEST_GOOGLE_PLAY_SERVICES = 1002
+        const val REQUEST_PERMISSION_GET_ACCOUNTS = 1003
+        const val PREF_ACCOUNT_NAME = "accountName";
 
-    setContentView(R.layout.activity_sign_in)
-
-    val signInButton = findViewById(R.id.signInButton)
-    signInButton.setOnClickListener {
-      Timber.i("logging in!")
-      NavigationUtils.goToSwipeableActivity(this)
+        final val SHEET_SCOPE: List<String> = listOf(SheetsScopes.SPREADSHEETS)
     }
-  }
+
+    var credential: GoogleAccountCredential? = null
+
+    override fun onCreate(savedInstanceBundle: Bundle?) {
+        super.onCreate(savedInstanceBundle)
+
+        setContentView(R.layout.activity_sign_in)
+
+//    val gso: GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//            .requestIdToken(getString(R.string.default_web_client_id))
+//            .requestEmail()
+//            .re
+//            .build();
+
+        val signInButton = findViewById(R.id.signInButton)
+        signInButton.setOnClickListener {
+            Timber.i("logging in!")
+//      NavigationUtils.goToSwipeableActivity(this)
+            getAccount()
+        }
+
+        credential = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), SHEET_SCOPE)
+                .setBackOff(ExponentialBackOff());
+    }
+
+    @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
+    private fun getAccount() {
+        if (EasyPermissions.hasPermissions(
+                this, android.Manifest.permission.GET_ACCOUNTS)) {
+            val accountName = getPreferences(Context.MODE_PRIVATE)
+                    .getString(PREF_ACCOUNT_NAME, null);
+            if (accountName != null) {
+                credential?.setSelectedAccountName(accountName);
+                getResultsFromApi();
+            } else {
+                // Start a dialog from which the user can choose an account
+                startActivityForResult(
+                        credential?.newChooseAccountIntent(),
+                        REQUEST_ACCOUNT_PICKER);
+            }
+        } else {
+            // Request the GET_ACCOUNTS permission via a user dialog
+            EasyPermissions.requestPermissions(
+                    this,
+                    "This app needs to access your Google account (via Contacts).",
+                    REQUEST_PERMISSION_GET_ACCOUNTS,
+                    android.Manifest.permission.GET_ACCOUNTS);
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data);
+        when(requestCode) {
+            REQUEST_ACCOUNT_PICKER -> {
+                if (resultCode == RESULT_OK && data != null &&
+                        data.getExtras() != null) {
+                    val accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME)
+                    if (accountName != null) {
+                        val settings: SharedPreferences = getPreferences(Context.MODE_PRIVATE)
+                        val editor = settings.edit()
+                        editor.putString(PREF_ACCOUNT_NAME, accountName);
+                        editor.apply();
+                        credential?.setSelectedAccountName(accountName);
+                        getResultsFromApi();
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getResultsFromApi() {
+        Timber.i("results from api")
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(
+                requestCode, permissions, grantResults, this);
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>?) {
+        //no op
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>?) {
+        //no op
+    }
+
 }
